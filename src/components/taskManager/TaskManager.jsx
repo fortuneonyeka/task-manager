943d3f06801023b6e2c217b2baa9e21227bffb15
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-10-27 12:36:30 +0100

    update taskmanager component with reactstrap Row and Column template

--- src/components/taskManager/TaskManager.jsx
+++ src/components/taskManager/TaskManager.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import TaskForm from "./taskForm/TaskForm";
+import TaskList from "./taskList/TaskList";
+import {  Row, Col } from "reactstrap";
 
-const TaskManager = props => {
+const TaskManager = (props) => {
   return (
-    <div>TaskManager</div>
-  )
-}
+    <>
+      <Row>
+        <Col>
+          <TaskForm />
+        </Col>
+        <Col>
+          <TaskList />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default TaskManager
+export default TaskManager;
