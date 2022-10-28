de708492b0d0e2bec37cb76f48b9cfb47b36b89a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-10-28 16:12:05 +0100

    created tasklist

--- src/components/taskManager/taskList/TaskList.jsx
+++ src/components/taskManager/taskList/TaskList.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import TaskItem from "./taskItem/TaskItem";
+import { ListGroup, Badge, Button } from "reactstrap";
+import Task from "../../models/Task";
 
 const TaskList = () => {
-  return (
-    <div>TaskList</div>
-  )
-}
+  const tasks = [
+    new Task(1, "code all day"),
+    new Task(2, "Learn React"),
+    new Task(3, "learn Ruby"),
+    new Task(4, "Learn rails"),
+    new Task(5, "Gym in the evening"),
+  ];
 
-export default TaskList
+  const allTasks = tasks.map((task) => {
+    return (
+      <div key={task.id}>
+        {task.title}
+
+        <Badge className="mx-3" color="success" pill>
+          14
+        </Badge>
+        <Button close />
+      </div>
+    );
+  });
+
+  return (
+    <ListGroup>
+      <h3>TaskList</h3>
+      <TaskItem allTasks={allTasks} />
+    </ListGroup>
+  );
+};
+
+export default TaskList;
