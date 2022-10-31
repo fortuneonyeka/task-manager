0f5a8739f35a8d4fc1b31a64d3fe851964a94042
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-10-31 13:57:15 +0100

    recieved prop task from taskmanger and pass it to taskitem

--- src/components/taskManager/taskList/TaskList.jsx
+++ src/components/taskManager/taskList/TaskList.jsx
@@ -1,34 +1,16 @@
 import React from "react";
 import TaskItem from "./taskItem/TaskItem";
-import { ListGroup, Badge, Button } from "reactstrap";
-import Task from "../../models/Task";
+import { ListGroup} from "reactstrap";
 
-const TaskList = () => {
-  const tasks = [
-    new Task(1, "code all day"),
-    new Task(2, "Learn React"),
-    new Task(3, "learn Ruby"),
-    new Task(4, "Learn rails"),
-    new Task(5, "Gym in the evening"),
-  ];
 
-  const allTasks = tasks.map((task) => {
-    return (
-      <div key={task.id}>
-        {task.title}
+const TaskList = ({task}) => {
+  
 
-        <Badge className="mx-3" color="success" pill>
-          14
-        </Badge>
-        <Button close />
-      </div>
-    );
-  });
 
   return (
     <ListGroup>
-      <h3>TaskList</h3>
-      <TaskItem allTasks={allTasks} />
+      
+       <TaskItem key={task.id} task={task}/>
     </ListGroup>
   );
 };
