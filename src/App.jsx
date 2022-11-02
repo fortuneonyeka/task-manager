b78d7b88d046c715f9886f4aa771af5e7b1dfda4
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-11-02 14:08:55 +0100

    add routing using react router with outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import Header from "./components/shared/header/Header";
 import Footer from "./components/shared/footer/Footer";
 import TaskManager from "./components/taskManager/TaskManager";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/login/Login";
+
+
 function App() {
   return (
     <div className="container">
-      <div className="row">
-          <div className="col">
-              <Header/>
-          </div>
-      </div>
-      <div className="row">
-          <div className="col">
-            <TaskManager/>
-          </div>
-      </div>
-      <div className="row">
-          <div className="col">
-              <Footer/>
-          </div>
-      </div>
+        <Header />
+      <Routes>
+        <Route path="/" element={<TaskManager />}></Route>
+
+        <Route path="login" element={<Login />}></Route>
+
+        <Route path="task" element={<TaskManager />} />
+
+      </Routes>
+        <Footer />
     </div>
   );
 }
--- src/components/taskManager/taskList/taskItem/TaskItem.jsx
+++ src/components/taskManager/taskList/taskItem/TaskItem.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { ListGroupItem, CloseButton } from "reactstrap";
 
-const TaskItem = ({task}) => {
- return(
-  <ListGroupItem>
-   
-    {task.title}
-    <CloseButton className="float-end"/>
-    </ListGroupItem> 
- )
+const TaskItem = ({ task }) => {
+  return (
+    <>
+      <ListGroupItem>
+        {task.title}
+        <CloseButton className="float-end" />
+      </ListGroupItem>
+    </>
+  );
 };
 
 export default TaskItem;
