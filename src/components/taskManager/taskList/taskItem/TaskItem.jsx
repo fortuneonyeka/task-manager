f718ee4149bc8ab7f25dfa507062fa5345b74e5a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-11-11 10:37:30 +0100

    dispatch DELETE_ACTION

--- src/components/taskManager/taskList/taskItem/TaskItem.jsx
+++ src/components/taskManager/taskList/taskItem/TaskItem.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { ListGroupItem, CloseButton } from "reactstrap";
+import { useDispatch } from "react-redux";
+import {deleteAction}  from "../../../../Redux/actions/TaskManagerActions";
+
 
 const TaskItem = ({ task }) => {
+  const dispatch = useDispatch()
+
+  
+  const handleDelete = () => {
+    const id  = task.id
+    dispatch(deleteAction(id));
+    
+  }
   return (
     <>
       <ListGroupItem>
         {task.title}
-        <CloseButton className="float-end bg-danger p-2" />
+        <CloseButton className="float-end bg-danger p-2" onClick={handleDelete}/>
       </ListGroupItem>
     </>
   );
