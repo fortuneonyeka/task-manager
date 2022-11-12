e627d39780fc7f2dc01aed3cbcad90f2e1613452
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-11-12 16:41:46 +0100

    add logger applyMiddleware

--- src/Redux/store/Store.js
+++ src/Redux/store/Store.js
@@ -1,6 +1,7 @@
-import {createStore} from "redux"
+import {createStore, applyMiddleware} from "redux"
 import rootReducer from "../reducers/rootReducer"
+import logger from "redux-logger" 
 
 
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(logger))
