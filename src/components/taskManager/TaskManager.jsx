fb56419279e4881add5a915d22d9825fc206f057
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-11-12 16:44:09 +0100

    install logger middleware

--- src/components/taskManager/TaskManager.jsx
+++ src/components/taskManager/TaskManager.jsx
@@ -1,42 +1,22 @@
-import React, {Component} from "react";
+import React from "react";
 import TaskForm from "./taskForm/TaskForm";
 import TaskList from "./taskList/TaskList";
-import {  Row, Col } from "reactstrap";
-import Task from "../models/Task";
-import {useDispatch} from "react-redux"
-import {addAction} from "../../Redux/actions/TaskManagerActions"
-class TaskManager extends Component  {
+import { Row, Col } from "reactstrap";
 
-  // taskAll = useSelector(state => state.tasks)
-  // dispatch = useDispatch()
-  
-   addTask = (data) => {
-    const task = new Task(
-      this.state.tasks.length + 1,
-      data.title,
-      data.description
-    )
-
-   this.setState({
-    tasks: [...this.state.tasks, task]
-   })
-  }
-  render() {
-    return (
-      <>
-        <Row className="task-manager-container">
-          <Col>
-            <TaskForm />
-          </Col>
-          <Col>
+const TaskManager = () => {
+  return (
+    <>
+      <Row className="task-manager-container">
+        <Col>
+          <TaskForm />
+        </Col>
+        <Col>
           <h3>TaskList</h3>
-           <TaskList /> 
-          </Col>
-        </Row>
-      </>
-    );
-  }
-  
+          <TaskList />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default TaskManager;
--- src/components/taskManager/taskList/taskItem/TaskItem.jsx
+++ src/components/taskManager/taskList/taskItem/TaskItem.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { ListGroupItem, CloseButton } from "reactstrap";
-import { useDispatch } from "react-redux";
-import {deleteAction}  from "../../../../Redux/actions/TaskManagerActions";
-
+import { useDispatch} from "react-redux";
+import { deleteAction } from "../../../../Redux/actions/TaskManagerActions";
 
 const TaskItem = ({ task }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  
   const handleDelete = () => {
-    const id  = task.id
-    dispatch(deleteAction(id));
-    
-  }
+    dispatch(deleteAction(task.id));
+  };
   return (
     <>
       <ListGroupItem>
         {task.title}
-        <CloseButton className="float-end bg-danger p-2" onClick={handleDelete}/>
+        <CloseButton
+          className="float-end bg-danger p-2"
+          onClick={handleDelete}
+        />
       </ListGroupItem>
     </>
   );
