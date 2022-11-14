a6d7ae169d7de414ec27a5117612545bff50d293
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-11-14 11:52:23 +0100

    setup json server

---
+++ src/services/TaskService.js
@@ -0,0 +1,16 @@
+import { CardTitle } from "reactstrap"
+
+export const getTask = () => {
+  
+}
+
+/**
+ * 
+ * @param {title, description} data
+ * */
+export const addTask = (data) => {
+
+}
+export const deleteTask = (id) => {
+
+}
